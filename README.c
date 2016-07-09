4671a3734c73fcc92dfeb7da870b6f23534672da
rloganayaki <loganayaki993@gmail.com>
2016-07-09 16:47:49 +0530

    finish

---
+++ README.c
@@ -0,0 +1,17 @@
+# loguuu
+#include<stdio.h>
+#include<conio.h>
+void main(0
+{
+int num;
+printf("enter the num");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("even num");
+}
+else
+{
+printf("odd num");
+}
+}
